d5e27dd0342a90387d1296d7d2cff190a45af866
Pedro H. Spagiari <phspagiari@gmail.com>
2020-02-04 13:10:48 -0300

    fix(pkg/checker) Adding Bearer Token header for API Server requests

--- pkg/checker/transport.go
+++ pkg/checker/transport.go
@@ -2,12 +2,32 @@ package checker
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
+)
+
+const (
+	tokenFile = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 )
 
 // doRequest does an http request only to get the http status code
 func (c *Checker) doRequest(url string) (string, error) {
-	resp, err := c.HTTPClient.Get(url)
+	// Read Bearer Token file from ServiceAccount
+	token, err := ioutil.ReadFile(tokenFile)
+	if err != nil {
+		fmt.Errorf("could not load token %s: %s", tokenFile, err)
+	}
+
+	client := c.HTTPClient
+	req, err := http.NewRequest("GET", url, nil)
+	// Only add the Bearer for API Server Requests
+	if strings.Contains(url, "/version") && token != nil {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return err.Error(), err
 	}
