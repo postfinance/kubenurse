2801ccbc68024a1e7082a13d7a16845f295c4b02
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2025-06-16 15:38:41 +0200

    test: improve GenMetrics and add benchmark
    as a followup to
    https://github.com/postfinance/kubenurse/pull/214#discussion_r2149847347

--- internal/util/metrics.go
+++ internal/util/metrics.go
@@ -1,21 +1,24 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const MetricsNamespace = "kubenurse"
 
 func GenMetricsName(name string, kvs ...string) string {
 	n := len(kvs)
-	labels := ""
+	labels := make([]string, n/2)
+
 	if n > 0 {
 		if n%2 != 0 {
 			panic("odd number or label tags, cannot construct the metric name")
 		}
 		for i := 0; i < n; i += 2 {
-			labels += fmt.Sprintf("%s=%q,", kvs[i], kvs[i+1])
+			labels[i/2] = fmt.Sprintf("%s=%q", kvs[i], kvs[i+1])
 		}
-		labels = labels[:len(labels)-1]
 	}
 
-	return fmt.Sprintf("%s_%s{%s}", MetricsNamespace, name, labels)
+	return fmt.Sprintf("%s_%s{%s}", MetricsNamespace, name, strings.Join(labels, ","))
 }
